4a6bd2b19cdff70ae999691719e1c263b44ec63d
LouisSetyandaru <121075836+LouisSetyandaru@users.noreply.github.com>
2024-05-10 15:04:23 +0700

    Initial Commit

---
+++ GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by MacBook Pro on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
