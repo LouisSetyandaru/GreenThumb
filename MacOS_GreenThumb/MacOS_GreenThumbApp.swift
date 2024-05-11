2f4010e00915951fe709a43c4511338f8ccc5950
LouisSetyandaru <121075836+LouisSetyandaru@users.noreply.github.com>
2024-05-11 19:14:24 +0700

    First Commit Set Up

---
+++ MacOS_GreenThumb/MacOS_GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_GreenThumbApp.swift
+//  MacOS_GreenThumb
+//
+//  Created by MacBook Pro on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchOS_GreenThumb Watch App/WatchOS_GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOS_GreenThumbApp.swift
+//  WatchOS_GreenThumb Watch App
+//
+//  Created by MacBook Pro on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_GreenThumb_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
