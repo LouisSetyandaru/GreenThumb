7eea44e6f6b8400ec4f2bb135cf66977c9518dff
monepdepresot <120069627+monepdepresot@users.noreply.github.com>
2024-05-22 16:47:59 +0700

    ayam bakar

---
+++ WatchOS_GreenThumb Watch App/PlantCardWatch.swift
@@ -0,0 +1,18 @@
+//
+//  PlantCardWatch.swift
+//  WatchOS_GreenThumb Watch App
+//
+//  Created by MacBook Pro on 22/05/24.
+//
+
+import SwiftUI
+
+struct PlantCardWatch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlantCardWatch()
+}
