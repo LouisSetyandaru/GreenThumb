ea31003e1e4843c530fca11bd09e42cb107a0b1c
monepdepresot <120069627+monepdepresot@users.noreply.github.com>
2024-05-22 16:49:17 +0700

    watchos
    watchos

--- WatchOS_GreenThumb Watch App/PlantCardWatch.swift
+++ WatchOS_GreenThumb Watch App/PlantCardWatch.swift
@@ -1,18 +1,65 @@
 //
-//  PlantCardWatch.swift
-//  WatchOS_GreenThumb Watch App
+//  PlantCard.swift
+//  GreenThumb
 //
-//  Created by MacBook Pro on 22/05/24.
+//  Created by STVN on 16/05/24.
 //
 
 import SwiftUI
 
 struct PlantCardWatch: View {
+    var plant: Plant
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            
+            HStack(alignment: .center) {
+                Text(plant.name)
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.28627450980392155, green: 0.5215686274509804, blue: 0.3254901960784314))
+                    .padding(.top, 40.0)
+                    
+                
+                
+                CircleImage(image: Image(plant.image))
+                    .frame(width: 1,height: 1)
+            }
+            .frame(width: 1, height: 1)
+            .background(Color(red: 0.286, green: 0.522, blue: 0.325).opacity(0.11))
+            .cornerRadius(15)
+
+              
+            HStack {
+                VStack(alignment: .leading) {
+                    
+                    Text(plant.description)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 20.0)
+                        .padding(.horizontal, 6)
+                        
+                }
+                
+            }
+            .frame(width: 1, height: 1)
+
+            
+        }
+        .background(Color(red: 0.286, green: 0.522, blue: 0.325).opacity(0.1))
+
+        .frame(width: 1, height: 1)
+        .cornerRadius(15)
     }
 }
 
 #Preview {
-    PlantCardWatch()
+    let plants = ModelData().plants
+    
+    return Group {
+        PlantCardWatch(plant: plants[1])
+
+    }
 }
+
+
+
