70672686d168bddeb885013572415e99a7935f49
Vamillaa <147577977+Vamillaa@users.noreply.github.com>
2024-05-30 12:32:44 +0700

    FormNama
    Seharusnya sudah siap

--- GreenThumb/FormUser.swift
+++ GreenThumb/FormUser.swift
@@ -2,14 +2,62 @@
 //  FormUser.swift
 //  GreenThumb
 //
-//  Created by student on 30/05/24.
+//  Created by EvanAC on 30/05/24.
 //
 
 import SwiftUI
 
 struct FormUser: View {
+    // Properties
+    @State private var nama = ""
+    // View
     var body: some View {
-        Text("yhygygy")
+        VStack() {
+            // Title
+            Text("GreenThumb")
+                .font(.largeTitle).foregroundColor(Color.white)
+                .padding([.top, .bottom], 40)
+                .shadow(radius: 10.0, x: 20, y: 10)
+            // Icon
+            Image("AppIcon")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10.0, x: 20, y: 10)
+                .padding(.bottom, 50)
+            // Name
+            VStack(alignment: .leading, spacing: 15) {
+                TextField("Name", text: self.$nama)
+                    .padding()
+                    .background(Color.themeTextField)
+                    .cornerRadius(20.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding([.leading, .trailing], 27.5)
+            // Continue
+            Button(action: {}) {
+                Text("Continue")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding(.top, 50)
+            
+            Spacer()
+
+        }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all))
+    }
+}
+
+extension Color {
+    static var themeTextField: Color {
+        return Color(red: 220.0/255.0, green: 230.0/255.0, blue: 230.0/255.0, opacity: 1.0)
     }
 }
 
