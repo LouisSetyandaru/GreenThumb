816047b39af32ce0cf00dc7890739dfcbc7f4cf8
Vamillaa <147577977+Vamillaa@users.noreply.github.com>
2024-05-30 12:21:37 +0700

    tambah Form user
    coba nambah foder

---
+++ GreenThumb/FormUser.swift
@@ -0,0 +1,18 @@
+//
+//  FormUser.swift
+//  GreenThumb
+//
+//  Created by student on 30/05/24.
+//
+
+import SwiftUI
+
+struct FormUser: View {
+    var body: some View {
+        Text("yhygygy")
+    }
+}
+
+#Preview {
+    FormUser()
+}
