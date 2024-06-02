895dc663d45c20d1f4c6d7ced4b2150c625ed083
michaeldavidsinn <146686816+michaeldavidsinn@users.noreply.github.com>
2024-06-02 19:37:44 +0700

    navigation mac, myplants mac, addedplantviewmac udah selesai, udah bisa add plant

---
+++ MacOS_GreenThumb/AddedPlantViewMac.swift
@@ -0,0 +1,62 @@
+//
+//  AddedPlantViewMac.swift
+//  MacOS_GreenThumb
+//
+//  Created by Michael Sin on 02/06/24.
+//
+
+import SwiftUI
+
+struct AddedPlantViewMac: View {
+    
+    var plant: Plant
+    
+    var body: some View {
+            
+            NavigationStack {
+                VStack(alignment: .center) {
+                    
+                    Spacer().frame(height: 270)
+                    
+                    Image("addedplant")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250, height: 150)
+                    
+                    Text("\(plant.name) has been added successfully! Remember to care for it—plants bring life to your home! 🌱")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                        .padding(.horizontal, 6)
+                        .padding(.bottom, 70)
+                        .frame(maxWidth: 350.0, alignment: .center)
+                        .multilineTextAlignment(.center)
+                    
+         
+                    
+                    NavigationLink(destination: HomeViewMac().environment(ModelData())) {
+                      
+                        Text("Back to Home")
+                            .padding(.vertical, 15.0)
+                            .padding(.horizontal, 80.0)
+                            .background(Color(red: 73/255, green: 133/255, blue: 83/255))
+                            .foregroundColor(Color.white)
+                            .cornerRadius(20)
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .background(Color.white)
+            
+        }
+}
+
+#Preview {
+    let plants = ModelData().plants
+    return Group {
+        AddedPlantViewMac(plant: plants[7])
+    }
+}
---
+++ MacOS_GreenThumb/MyPlantsViewMac.swift
@@ -0,0 +1,100 @@
+//
+//  MyPlantsViewMac.swift
+//  MacOS_GreenThumb
+//
+//  Created by Michael Sin on 02/06/24.
+//
+
+import SwiftUI
+
+struct MyPlantsViewMac: View {
+    
+    @Environment(ModelData.self) var modelData
+    @State private var searchText = ""
+
+    
+    var filteredPlants: [Plant] {
+        modelData.plants.filter { $0.isOnList == true }
+    }
+    
+    var body: some View {
+        
+        NavigationStack {
+            VStack {
+                // Top bar
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        TextField("Search", text: .constant(""))
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .frame(height: 30)
+                    }
+                    .padding(.horizontal, 10)
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    
+                    Spacer()
+                    
+                    // Home button
+                    NavigationLink(destination: HomeViewMac()) {
+                        Text("Home")
+                            .foregroundColor(.white)
+                            .frame(width: 80, height: 40)
+                            .background(Color(red: 199/255, green: 219/255, blue: 199/255))
+                            .cornerRadius(15)
+                    }.cornerRadius(15)
+                    
+                    // Profile button
+                    Button(action: {
+                        // Action for the profile button
+                    }) {
+                        Text("Profile")
+                            .foregroundColor(.white)
+                            .frame(width: 80, height: 40)
+                            .background(Color(red: 199/255, green: 219/255, blue: 199/255))
+                            .cornerRadius(15)
+                    }.cornerRadius(15)
+                    
+                    // Notification button
+                    Button(action: {
+                        // Action for the notification button
+                    }) {
+                        Image(systemName: "bell.fill")
+                            .foregroundColor(Color(red: 199/255, green: 219/255, blue: 199/255))
+                            .frame(width: 40, height: 40)
+                    }.cornerRadius(15)
+                }
+                .padding()
+                
+                ScrollView {
+                    
+                    Text("My Plants View")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .padding()
+                        .foregroundColor(Color(red: 73/255, green: 133/255, blue: 83/255))
+                    
+                    Spacer().padding()
+                    
+                    
+                    VStack {
+                        ForEach(filteredPlants) { plant in
+                            NavigationLink(destination: DetailViewMac(plant: plant)) {
+                                PlantCardMacOS(plant: plant)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                .background(Color(red: 249/255, green: 249/255, blue: 249/255))
+                .frame(minWidth: 1080, minHeight: 720)
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    MyPlantsViewMac().environment(ModelData())
+}
