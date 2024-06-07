2f59eef2b7b7f25966327022f94b1c6c8091e4af
michaeldavidsinn <146686816+michaeldavidsinn@users.noreply.github.com>
2024-06-07 23:49:15 +0700

    fix macos

--- MacOS_GreenThumb/MacOS_GreenThumbApp.swift
+++ MacOS_GreenThumb/MacOS_GreenThumbApp.swift
@@ -2,16 +2,18 @@
 //  MacOS_GreenThumbApp.swift
 //  MacOS_GreenThumb
 //
-//  Created by MacBook Pro on 11/05/24.
+//  Created by Michael Sin on 07/06/24.
 //
 
 import SwiftUI
 
 @main
-struct MacOS_GreenThumbApp: App {
+struct MacOS_GreenThumb_Watch_AppApp: App {
+    
+    @State private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(modelData)
         }
     }
 }
